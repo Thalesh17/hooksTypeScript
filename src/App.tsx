f6aacc2e25e9d9c805a4a188c52146d7f3a97530
Thales Henrique <thaales_jp@hotmail.com>
2020-07-14 11:16:59 -0300

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+
+interface User {
+  name: string;
+  login: string;
+  avatar_url: string
+}
 
 function App() {
+  const [user, setUser] = useState<User>();
+  
+  async function loadData() {
+    const response = await fetch('https://api.github.com/users/Thalesh17');
+    const data = await response.json();
+
+    setUser(data);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user?.name}
     </div>
   );
 }
